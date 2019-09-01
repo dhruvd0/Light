cdb363093537a397ab97d3ea793b9a9c39b38063
dhruv-01000100 <dhruvddevasthale@hotmail.com>
2019-09-01 14:40:13 +0530

    Main Function

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print ("Hello World")
+def Main():  # all the functions will be called here
+    print("starting from main ")
+    
+if __name__ == '__main__':  # executes the block at the start of the program , in this case it will execute Main()
+    Main()
