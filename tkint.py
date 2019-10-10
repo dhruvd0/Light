9c168935ac8faf31edb82536337cfb1253f2774f
Kanishk Chauhan <chauhankanishk990@gmail.com>
2019-10-10 21:09:02 +0530

    suggestion buttons and labels
    made suggestion buttons
    made labels
    made buttons for time table and calender

--- tkint.py
+++ tkint.py
@@ -1,20 +1,13 @@
 import tkinter as tk
-import os
-import notifs
-import webbrowser
-import requests
-import urllib
-import web
-from getpass import getpass
-from bs4 import BeautifulSoup
-import wget
-import tkint
-import lms
+#import lms
 
 root = tk.Tk()
 
 HEIGHT = 1080
 WIDTH = 1920
+
+def main_input(say):
+    print(say)
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='black')
 canvas.pack()
@@ -37,24 +30,27 @@ frame_tt.place(rely=0.2, relwidth=0.2, relheight=0.6)
 frame_image = tk.Frame(root, bg='white')
 frame_image.place(relx=0.37, rely=0.01, relwidth=0.2, relheight=0.1)
 
-frame_sugg = tk.Frame(root, bg='white')
+frame_sugg = tk.Frame(root, bg='black')
 frame_sugg.place(rely=0.85, relwidth=1, relheight=0.1)
 
 
 label_logo = tk.Label(frame_image, text="LOGO", fg='black', font=30)
 label_logo.pack()
 
-label_tt = tk.Label(frame_tt, text="TIME TABLE", fg='black', font=30)
-label_tt.pack()
+label_tt = tk.Label(frame_tt,text = "Time table", bg = 'green', font = 20)
+label_tt.place(rely = 0.1, relheight = 1 , relwidth = 1)
 
 label_dline = tk.Label(frame_dline, text="DEADLINES", fg='black', font=30)
-label_dline.pack()
+label_dline.place(relheight=1,relwidth=1)
 
-label_cal = tk.Label(frame_cal, text="CALENDER", fg='black', font=30)
-label_cal.pack()
+label_cal1 = tk.Label(frame_cal,text = "CALENDER", font = 15)
+label_cal1.place(relx=0.08,relheight= 0.1, relwidth=0.5)
 
-label_sugg = tk.Label(frame_sugg, text="SUGGESTIONS", fg='black', font=30)
-label_sugg.pack()
+label_cal2 = tk.Label(frame_cal)
+label_cal2.place(rely = 0.1 , relheight=0.9, relwidth=1)
+
+label_sugg = tk.Label(frame_sugg, text = 'Suggestions: ', font = 20, bg = 'black', fg = 'white')
+label_sugg.place(relwidth = 0.14 , relheight = 0.3)
 
 label_main = tk.Label(frame_display)
 label_main.place(relwidth = 1, relheight= 0.95 )
@@ -74,19 +70,53 @@ entry_pass = tk.Entry(frame, bg='#1f1f14', fg='white')
 entry_pass.pack()
 
 def login():
-    lms.loginLms(entry_user.get(), entry_pass.get())
+    #lms.loginLms(entry_user.get(), entry_pass.get())
+    pass
 
 
 entry_main = tk.Entry(frame_display, bg = 'white', fg='black' )
 entry_main.place(rely = 0.94, relwidth=0.9 , relheight = 0.06)
 
-button_main = tk.Button(frame_display,text = "-->", bg = 'black', fg = 'white', command = lambda : main_input(entry_main.get()))
+#button for main display ---->
+
+button_main = tk.Button(frame_display,text = "-->", bg = 'black', fg = 'white',activebackground = 'black', activeforeground= 'white', command = lambda : main_input(entry_main.get()))
 button_main.place(rely=0.94, relx = 0.9, relwidth = 0.1, relheight = 0.06)
 
+#button for submit ------> 
 
 button = tk.Button(frame, text="Submit", bg='#1f1f14',
-                   fg='white', command=login)
+                   fg='white', command=login,activebackground = 'black', activeforeground= 'white')
 button.pack()
+
+#buttons for calender ----->
+
+button_cal1 = tk.Button(frame_cal, text = "<--", bg = '#1f1f14', fg = 'white', activebackground = 'black', activeforeground= 'white')
+button_cal1.place(relx=0.0001, relheight = 0.1, relwidth= 0.1)
+
+button_cal1 = tk.Button(frame_cal, text = "-->", bg = '#1f1f14', fg = 'white', activebackground = 'black', activeforeground= 'white')
+button_cal1.place(relx=0.55, relheight = 0.1, relwidth= 0.1)
+
+#buttons for time table ------->
+
+button_tt1 = tk.Button(frame_tt,text = '<--', bg = '#1f1f14', fg = 'white', activebackground = 'black', activeforeground= 'white' )
+button_tt1.place(relheight=0.1, relwidth = 0.15)
+
+button_tt2 = tk.Button(frame_tt,text = '-->', bg = '#1f1f14', fg = 'white', activebackground = 'black', activeforeground= 'white' )
+button_tt2.place(relx= 0.85, relheight=0.1, relwidth = 0.15)
+
+#suggestion buttons ------>
+
+button_sugg1 = tk.Button(frame_sugg, text = 'Suggestion 1', bg = '#1f1f14', fg = 'white', activebackground = 'black', activeforeground= 'white')
+button_sugg1.place(relx= 0.05, rely=0.45,relheight= 0.4,relwidth=0.15)
+
+button_sugg2 = tk.Button(frame_sugg, text = 'Suggestion 2', bg = '#1f1f14', fg = 'white',activebackground = 'black', activeforeground= 'white')
+button_sugg2.place(relx= 0.25,rely=0.45,relheight= 0.4,relwidth=0.15)
+
+button_sugg3 = tk.Button(frame_sugg, text = 'Suggestion 3', bg = '#1f1f14', fg = 'white',activebackground = 'black', activeforeground= 'white')
+button_sugg3.place(relx=0.45 ,rely=0.45,relheight= 0.4,relwidth=0.15)
+
+button_sugg4 = tk.Button(frame_sugg, text = 'Suggestion 4', bg = '#1f1f14', fg = 'white',activebackground = 'black', activeforeground= 'white')
+button_sugg4.place(relx=0.65, rely=0.45,relheight= 0.4,relwidth=0.15)
 
 while True:
     root.update()
